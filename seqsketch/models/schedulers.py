57bd668bad795739d86102a9ca39d784eae605e9
Kasperjoergensen3 <Kasperjoergensen3@gmail.com>
2024-11-10 17:57:05 +0900

    updated scheduler

--- seqsketch/models/schedulers.py
+++ seqsketch/models/schedulers.py
@@ -3,4 +3,10 @@ from diffusers.schedulers import DDIMScheduler
 
 class myDDIMScheduler(DDIMScheduler):
     def __init__(self, params):
-        super().__init__(**params.__dict__)
+        super().__init__(
+            num_train_timesteps=params.num_train_timesteps,
+            beta_start=params.beta_start,
+            beta_end=params.beta_end,
+            beta_schedule=params.beta_schedule,
+            clip_sample=params.clip_sample,
+        )
